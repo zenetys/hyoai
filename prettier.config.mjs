ae94a014f5d249d10bbff822d402cc9603b0d8f1
Sylvain LAMBERT <slambert@zenetys.com>
2026-06-30 11:41:00 +0200

    BUILD: Add package.json and project configs

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+const prettierConfig = {
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    trailingComma: "all",
+    printWidth: 100,
+};
+
+export default prettierConfig;
